e01b4417fdcd7750c35173e72f1bc0d84b53782f
kvanttiapina <antsa013@gmail.com>
2014-08-06 14:15:04 +0100

    further refinements

--- qml/Track.js
+++ qml/Track.js
@@ -9,6 +9,7 @@
 
 var errInd = '*';
 
+
 var controlProto = {}
 
 controlProto.toggleTracking = function () {
@@ -20,8 +21,9 @@ controlProto.toggleTracking = function () {
     this._state_data.nofix.duration = 0;
     this._state = 'start';
     this._started = Date.now();
-    this._track_id = this._storage.createTrack();
-    this._last_update = Date.now();
+    this._track_id = this._storage.createTrack()
+    this._last_update = Date.now()
+    this._history.init()
 }
 
 
@@ -29,7 +31,7 @@ controlProto.updateTrack = function(pos, valid) {
     if (!this.tracking) return
     var now = Date.now()
     if (!valid) {
-        this._transition('nofix', now);
+        this._transition('nofix', now, pos);
         return;
     }
     this._state_data[this._state].execute(pos, now);
@@ -86,30 +88,21 @@ controlProto.bearing = function() {
 
 
 controlProto._add_pos = function(pos) {
-    var moved = pos.coordinate.distanceTo(this._last_pos.coord())
-
+    var moved = pos.coordinate.distanceTo(this._last_stored_pos.coord())
     if (moved < this._minimum_moving_distance || moved > this._maximum_moving_distance) {
         return
     }
-    var v_alt = pos.altitudeValid ? pos.coordinate.altitude : false;
-    var v_vacc = pos.verticalAccuracyValid ? pos.verticalAccuracy : false;
-    this._storage.addPosition(this._track_id,
-                              pos.timestamp,
-                              pos.coordinate.latitude,
-                              pos.coordinate.longitude,
-                              v_alt,
-                              pos.horizontalAccuracy,
-                              v_vacc);
-
+    this._add_pos_common(pos)
     this._records += 1;
-
-    this._last_pos.stamp = pos.timestamp;
-    this._last_pos.lat = pos.coordinate.latitude;
-    this._last_pos.lon = pos.coordinate.longitude;
-
 }
 
 controlProto._add_first_pos = function(pos) {
+    this._add_pos_common(pos)
+    this._records = 1;
+
+}
+
+controlProto._add_pos_common = function(pos) {
     var v_alt = pos.altitudeValid ? pos.coordinate.altitude : false;
     var v_vacc = pos.verticalAccuracyValid ? pos.verticalAccuracy : false;
     this._storage.addPosition(this._track_id,
@@ -119,11 +112,10 @@ controlProto._add_first_pos = function(pos) {
                   v_alt,
                   pos.horizontalAccuracy,
                   v_vacc);
-    this._records = 1;
 
-    this._last_pos.stamp = pos.timestamp;
-    this._last_pos.lat = pos.coordinate.latitude;
-    this._last_pos.lon = pos.coordinate.longitude;
+    this._last_stored_pos.stamp = pos.timestamp;
+    this._last_stored_pos.lat = pos.coordinate.latitude;
+    this._last_stored_pos.lon = pos.coordinate.longitude;
 
 }
 
@@ -132,36 +124,43 @@ controlProto._check_pos = function(pos) {
 }
 
 controlProto._is_stationary = function(pos) {
-    var moved = pos.coordinate.distanceTo(this._last_pos.coord())
-    if (moved < pos.horizontalAccuracy / 2) return true
-    var in_move = this._history.current() === 'moving' || this._history.previous() === 'moving'
-    if (moved < pos.horizontalAccuracy) {
-        if (in_move) return false
-        return true
+    var curr_state = this._history.previous(0).state
+    // console.log(curr_state)
+    var oldest = this._history.previous(0)
+    var hlen = this._history.length - 4
+    for (var idx = 1; idx < hlen; idx++) {
+        var prev = this._history.previous(idx)
+        if (prev.state !== curr_state) {
+            break
+        }
+        oldest = prev
     }
+    if (oldest.state === 'moving' && idx < hlen) {
+        oldest = this._history.previous(idx+1)
+    }
+
+    var moved = oldest.pos.coordinate.distanceTo(pos.coordinate)
+    // console.log('' + 4*moved + ' / ' + idx)
+    if (moved < pos.horizontalAccuracy / 4) return true
     return false
 }
 
 controlProto._update_moving = function(pos) {
-    var moved = pos.coordinate.distanceTo(this._last_pos.coord())
-
-    if (moved < this._minimum_moving_distance || moved > this._maximum_moving_distance) {
-        return
-    }
-
-    var dist = pos.coordinate.distanceTo(this._last_pos.coord());
-    var duration = (pos.timestamp - this._last_pos.stamp) / 1000;
+    // TODO least squares fit
+    var prev = this._history.previous(0).pos
+    var dist = pos.coordinate.distanceTo(prev.coordinate);
+    var duration = (pos.timestamp - prev.timestamp) / 1000;
     this._distance += dist;
     this._speed = dist / duration;
-    this._bearing = this._last_pos.coord().azimuthTo(pos.coordinate);
+    this._bearing = prev.coordinate.azimuthTo(pos.coordinate);
 }
 
 
-controlProto._transition = function(to, tm) {
-    this._history.update(to)
-    this._state_data[this._state].duration = this._state_data[this._state].duration + (tm - this._last_update);
-    this._state = to;
-    this._last_update = tm;
+controlProto._transition = function(to, tm, pos) {
+    this._history.update(to, pos)
+    this._state_data[this._state].duration += tm - this._last_update
+    this._state = to
+    this._last_update = tm
 }
 
 
@@ -172,7 +171,7 @@ controlProto._transition = function(to, tm) {
 var nofixProto = {}
 nofixProto.execute = function(pos, now) {
     if (!this.parent._check_pos(pos)) {
-        this.parent._transition('nofix', now)
+        this.parent._transition('nofix', now, pos)
         return;
     }
     if (this.parent._records === 0) {
@@ -180,37 +179,37 @@ nofixProto.execute = function(pos, now) {
     } else {
         this.parent._add_pos(pos);
     }
-    this.parent._transition('stationary', now);
+    this.parent._transition('stationary', now, pos);
 }
 
 var stationaryProto = {}
 stationaryProto.execute = function(pos, now) {
     if (!this.parent._check_pos(pos)) {
-        this.parent._transition('nofix', now)
+        this.parent._transition('nofix', now, pos)
         return
     }
     if (this.parent._is_stationary(pos)) {
-        this.parent._transition('stationary', now)
+        this.parent._transition('stationary', now, pos)
         return
     }
     this.parent._update_moving(pos)
     this.parent._add_pos(pos)
-    this.parent._transition('moving', now)
+    this.parent._transition('moving', now, pos)
 }
 
 var movingProto = {}
 movingProto.execute = function(pos, now) {
     if (!this.parent._check_pos(pos)) {
-        this.parent._transition('nofix', now)
+        this.parent._transition('nofix', now, pos)
         return
     }
     if (this.parent._is_stationary(pos)) {
-        this.parent._transition('stationary', now)
+        this.parent._transition('stationary', now, pos)
         return
     }
     this.parent._update_moving(pos);
     this.parent._add_pos(pos)
-    this.parent._transition('moving', now);
+    this.parent._transition('moving', now, pos);
 }
 
 
@@ -220,28 +219,28 @@ movingProto.execute = function(pos, now) {
 
 var historyProto = {}
 
-historyProto.init = function(len) {
+historyProto.init = function() {
     this._history = []
-    this._len = len
+    this.length = 10
     this._current = 0
-    for (var idx = 0; idx < len; idx++) {
-        this._history.push('start')
+    for (var idx = 0; idx < this.length; idx++) {
+        this._history.push({state: 'start', pos: {timestamp: 0, coordinate: P.QtPositioning.coordinate(0,0)}})
     }
 }
 
-historyProto.update = function (state) {
-    this._current = (this._current + 1) % this._len
-    this._history[this._current] = state
+historyProto.update = function (state, pos) {
+    this._current = (this._current + 1) % this.length
+    this._history[this._current].state = state
+    this._history[this._current].pos.timestamp = pos.timestamp
+    this._history[this._current].pos.coordinate = P.QtPositioning.coordinate(pos.coordinate.latitude,
+                                                                             pos.coordinate.longitude)
 }
 
-historyProto.previous = function () {
-    var pre = (this._current - 1 + this._len) % this._len
+historyProto.previous = function (idx) {
+    var pre = (this._current - idx + this.length) % this.length
     return this._history[pre]
 }
 
-historyProto.current = function () {
-    return this._history[this._current]
-}
 
 // ------------------------------
 // Constructors
@@ -266,29 +265,29 @@ function _createStates(parent) {
 
 function createController() {
     tracker = G.create(controlProto)
-    tracker._state = 'start';
-    tracker.tracking = false;
-    tracker._track_id = 0;
-    tracker._started = new Date();
-    tracker._records = 0;
-    tracker._distance = 0;
-    tracker._speed = 0;
-    tracker._bearing = 0;
+    tracker._state = 'start'
+    tracker.tracking = false
+    tracker._track_id = 0
+    tracker._started = new Date()
+    tracker._records = 0
+    tracker._distance = 0
+    tracker._speed = 0
+    tracker._bearing = 0
     tracker._last_update = Date.now()
-    tracker._minimum_moving_distance = 5.0;
-    tracker._maximum_moving_distance = 100.0;
+    tracker._minimum_moving_distance = 5.0
+    tracker._maximum_moving_distance = 100.0
 
     tracker._state_data = _createStates(tracker)
-    tracker._last_pos = {
+    tracker._last_stored_pos = {
         stamp: Date.now(),
         lat: 0.0,
         lon: 0.0,
         coord: function() {return P.QtPositioning.coordinate(this.lat, this.lon)},
-    };
-    tracker._storage = S.createController();
-    tracker.formatter = F.createFormatter();
+    }
+    tracker._storage = S.createController()
+    tracker.formatter = F.createFormatter()
     tracker._history = G.create(historyProto)
-    tracker._history.init(5)
+    tracker._history.init()
 
-    return tracker;
+    return tracker
 }
